feae22c01e9cd351450bb534d9dfed56eae7b392
hhhxxyH <2294470646@qq.com>
2026-08-21 03:10:23 +0800

    繁简学堂：组模式学习与 PWA 部署

---
+++ serve.js
@@ -0,0 +1,62 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+
+const ROOT = path.join(__dirname, "app");
+const PORT = Number(process.env.PORT || 4173);
+
+const MIME = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".js": "application/javascript; charset=utf-8",
+  ".json": "application/json; charset=utf-8",
+  ".webmanifest": "application/manifest+json; charset=utf-8",
+  ".svg": "image/svg+xml",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".ico": "image/x-icon"
+};
+
+const server = http.createServer(function (req, res) {
+  let urlPath = decodeURIComponent((req.url || "/").split("?")[0]);
+  if (urlPath === "/") urlPath = "/index.html";
+
+  const filePath = path.normalize(path.join(ROOT, urlPath));
+  if (!filePath.startsWith(ROOT)) {
+    res.writeHead(403, { "Content-Type": "text/plain; charset=utf-8" });
+    res.end("Forbidden");
+    return;
+  }
+
+  fs.readFile(filePath, function (err, data) {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("Not Found");
+      return;
+    }
+    const ext = path.extname(filePath).toLowerCase();
+    res.writeHead(200, { "Content-Type": MIME[ext] || "application/octet-stream" });
+    res.end(data);
+  });
+});
+
+function lanUrls() {
+  const urls = [];
+  const ifaces = os.networkInterfaces();
+  Object.keys(ifaces).forEach(function (name) {
+    ifaces[name].forEach(function (info) {
+      if (info.family === "IPv4" && !info.internal) {
+        urls.push("http://" + info.address + ":" + PORT);
+      }
+    });
+  });
+  return urls;
+}
+
+server.listen(PORT, function () {
+  console.log("繁简学堂已启动：http://localhost:" + PORT);
+  lanUrls().forEach(function (url) {
+    console.log("手机在同一网络下访问：" + url);
+  });
+});
